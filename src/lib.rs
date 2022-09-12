84f2397e0a0faa3ffa7cd1b1708b81813a39213c
Cathie So <socathie@gmail.com>
2022-09-12 23:53:43 +0800

    remove wasmer dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-#![doc = include_str!("../README.md")]
+// #![doc = include_str!("../README.md")]
 #![warn(clippy::all, clippy::pedantic, clippy::cargo, clippy::nursery)]
 // TODO: ark-circom and ethers-core pull in a lot of dependencies, some duplicate.
 #![allow(clippy::multiple_crate_versions)]
 
-mod circuit;
+// mod circuit;
 mod field;
 pub mod hash;
 pub mod identity;
@@ -28,7 +28,7 @@ pub use crate::field::{hash_to_field, Field};
 pub use circuit::initialize;
 
 pub type Groth16Proof = ark_groth16::Proof<Bn<Parameters>>;
-pub type EthereumGroth16Proof = ark_circom::ethereum::Proof;
+// pub type EthereumGroth16Proof = ark_circom::ethereum::Proof;
 
 #[cfg(test)]
 mod test {
@@ -36,76 +36,76 @@ mod test {
         hash_to_field,
         identity::Identity,
         poseidon_tree::PoseidonTree,
-        protocol::{generate_nullifier_hash, generate_proof, verify_proof},
+        protocol::{generate_nullifier_hash},
         Field,
     };
     use std::thread::spawn;
 
-    #[test]
-    fn test_field_serde() {
-        let value = Field::from(0x1234_5678);
-        let serialized = serde_json::to_value(value).unwrap();
-        let deserialized = serde_json::from_value(serialized).unwrap();
-        assert_eq!(value, deserialized);
-    }
+    // #[test]
+    // fn test_field_serde() {
+    //     let value = Field::from(0x1234_5678);
+    //     let serialized = serde_json::to_value(value).unwrap();
+    //     let deserialized = serde_json::from_value(serialized).unwrap();
+    //     assert_eq!(value, deserialized);
+    // }
 
-    fn test_end_to_end(identity: &[u8], external_nullifier: &[u8], signal: &[u8]) {
-        // const LEAF: Hash = Hash::from_bytes_be(hex!(
-        //     "0000000000000000000000000000000000000000000000000000000000000000"
-        // ));
-        let leaf = Field::from(0);
+    // fn test_end_to_end(identity: &[u8], external_nullifier: &[u8], signal: &[u8]) {
+    //     // const LEAF: Hash = Hash::from_bytes_be(hex!(
+    //     //     "0000000000000000000000000000000000000000000000000000000000000000"
+    //     // ));
+    //     let leaf = Field::from(0);
 
-        // generate identity
-        let id = Identity::from_seed(identity);
+    //     // generate identity
+    //     let id = Identity::from_seed(identity);
 
-        // generate merkle tree
-        let mut tree = PoseidonTree::new(21, leaf);
-        tree.set(0, id.commitment());
+    //     // generate merkle tree
+    //     let mut tree = PoseidonTree::new(21, leaf);
+    //     tree.set(0, id.commitment());
 
-        let merkle_proof = tree.proof(0).expect("proof should exist");
-        let root = tree.root();
+    //     let merkle_proof = tree.proof(0).expect("proof should exist");
+    //     let root = tree.root();
 
-        let signal_hash = hash_to_field(signal);
-        let external_nullifier_hash = hash_to_field(external_nullifier);
-        let nullifier_hash = generate_nullifier_hash(&id, external_nullifier_hash);
+    //     let signal_hash = hash_to_field(signal);
+    //     let external_nullifier_hash = hash_to_field(external_nullifier);
+    //     let nullifier_hash = generate_nullifier_hash(&id, external_nullifier_hash);
 
-        let proof =
-            generate_proof(&id, &merkle_proof, external_nullifier_hash, signal_hash).unwrap();
+    //     let proof =
+    //         generate_proof(&id, &merkle_proof, external_nullifier_hash, signal_hash).unwrap();
 
-        for _ in 0..5 {
-            let success = verify_proof(
-                root,
-                nullifier_hash,
-                signal_hash,
-                external_nullifier_hash,
-                &proof,
-            )
-            .unwrap();
-            assert!(success);
-        }
-    }
-    #[test]
-    fn test_single() {
-        // Note that rust will still run tests in parallel
-        test_end_to_end(b"hello", b"appId", b"xxx");
-    }
+    //     for _ in 0..5 {
+    //         let success = verify_proof(
+    //             root,
+    //             nullifier_hash,
+    //             signal_hash,
+    //             external_nullifier_hash,
+    //             &proof,
+    //         )
+    //         .unwrap();
+    //         assert!(success);
+    //     }
+    // }
+    // #[test]
+    // fn test_single() {
+    //     // Note that rust will still run tests in parallel
+    //     test_end_to_end(b"hello", b"appId", b"xxx");
+    // }
 
-    #[test]
-    fn test_parallel() {
-        // Note that this does not guarantee a concurrency issue will be detected.
-        // For that we need much more sophisticated static analysis tooling like
-        // loom. See <https://github.com/tokio-rs/loom>
-        let a = spawn(|| test_end_to_end(b"hello", b"appId", b"xxx"));
-        let b = spawn(|| test_end_to_end(b"secret", b"test", b"signal"));
-        a.join().unwrap();
-        b.join().unwrap();
-    }
+    // #[test]
+    // fn test_parallel() {
+    //     // Note that this does not guarantee a concurrency issue will be detected.
+    //     // For that we need much more sophisticated static analysis tooling like
+    //     // loom. See <https://github.com/tokio-rs/loom>
+    //     let a = spawn(|| test_end_to_end(b"hello", b"appId", b"xxx"));
+    //     let b = spawn(|| test_end_to_end(b"secret", b"test", b"signal"));
+    //     a.join().unwrap();
+    //     b.join().unwrap();
+    // }
 }
 
 #[cfg(feature = "bench")]
 pub mod bench {
     use crate::{
-        hash_to_field, identity::Identity, poseidon_tree::PoseidonTree, protocol::generate_proof,
+        hash_to_field, identity::Identity, poseidon_tree::PoseidonTree,
         Field,
     };
     use criterion::Criterion;
@@ -118,23 +118,23 @@ pub mod bench {
         bench_proof(criterion);
     }
 
-    fn bench_proof(criterion: &mut Criterion) {
-        let leaf = Field::from(0);
+    // fn bench_proof(criterion: &mut Criterion) {
+    //     let leaf = Field::from(0);
 
-        // Create tree
-        let id = Identity::from_seed(b"hello");
-        let mut tree = PoseidonTree::new(21, leaf);
-        tree.set(0, id.commitment());
-        let merkle_proof = tree.proof(0).expect("proof should exist");
+    //     // Create tree
+    //     let id = Identity::from_seed(b"hello");
+    //     let mut tree = PoseidonTree::new(21, leaf);
+    //     tree.set(0, id.commitment());
+    //     let merkle_proof = tree.proof(0).expect("proof should exist");
 
-        // change signal and external_nullifier here
-        let signal_hash = hash_to_field(b"xxx");
-        let external_nullifier_hash = hash_to_field(b"appId");
+    //     // change signal and external_nullifier here
+    //     let signal_hash = hash_to_field(b"xxx");
+    //     let external_nullifier_hash = hash_to_field(b"appId");
 
-        criterion.bench_function("proof", move |b| {
-            b.iter(|| {
-                generate_proof(&id, &merkle_proof, external_nullifier_hash, signal_hash).unwrap();
-            });
-        });
-    }
+    //     criterion.bench_function("proof", move |b| {
+    //         b.iter(|| {
+    //             generate_proof(&id, &merkle_proof, external_nullifier_hash, signal_hash).unwrap();
+    //         });
+    //     });
+    // }
 }
--- src/protocol.rs
+++ src/protocol.rs
@@ -1,5 +1,5 @@
 use crate::{
-    circuit::{witness_calculator, zkey},
+    // circuit::{witness_calculator, zkey},
     identity::Identity,
     merkle_tree::{self, Branch},
     poseidon,
@@ -7,7 +7,7 @@ use crate::{
     Field,
 };
 use ark_bn254::{Bn254, Fr, Parameters};
-use ark_circom::CircomReduction;
+// use ark_circom::CircomReduction;
 use ark_ec::bn::Bn;
 use ark_groth16::{
     create_proof_with_reduction_and_matrices, prepare_verifying_key, Proof as ArkProof,
@@ -31,35 +31,35 @@ pub type G2 = ([U256; 2], [U256; 2]);
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Proof(G1, G2, G1);
 
-impl From<ArkProof<Bn<Parameters>>> for Proof {
-    fn from(proof: ArkProof<Bn<Parameters>>) -> Self {
-        let proof = ark_circom::ethereum::Proof::from(proof);
-        let (a, b, c) = proof.as_tuple();
-        Self(a, b, c)
-    }
-}
+// impl From<ArkProof<Bn<Parameters>>> for Proof {
+//     fn from(proof: ArkProof<Bn<Parameters>>) -> Self {
+//         let proof = ark_circom::ethereum::Proof::from(proof);
+//         let (a, b, c) = proof.as_tuple();
+//         Self(a, b, c)
+//     }
+// }
 
-impl From<Proof> for ArkProof<Bn<Parameters>> {
-    fn from(proof: Proof) -> Self {
-        let eth_proof = ark_circom::ethereum::Proof {
-            a: ark_circom::ethereum::G1 {
-                x: proof.0 .0,
-                y: proof.0 .1,
-            },
-            #[rustfmt::skip] // Rustfmt inserts some confusing spaces
-            b: ark_circom::ethereum::G2 {
-                // The order of coefficients is flipped.
-                x: [proof.1.0[1], proof.1.0[0]],
-                y: [proof.1.1[1], proof.1.1[0]],
-            },
-            c: ark_circom::ethereum::G1 {
-                x: proof.2 .0,
-                y: proof.2 .1,
-            },
-        };
-        eth_proof.into()
-    }
-}
+// impl From<Proof> for ArkProof<Bn<Parameters>> {
+//     fn from(proof: Proof) -> Self {
+//         let eth_proof = ark_circom::ethereum::Proof {
+//             a: ark_circom::ethereum::G1 {
+//                 x: proof.0 .0,
+//                 y: proof.0 .1,
+//             },
+//             #[rustfmt::skip] // Rustfmt inserts some confusing spaces
+//             b: ark_circom::ethereum::G2 {
+//                 // The order of coefficients is flipped.
+//                 x: [proof.1.0[1], proof.1.0[0]],
+//                 y: [proof.1.1[1], proof.1.1[0]],
+//             },
+//             c: ark_circom::ethereum::G1 {
+//                 x: proof.2 .0,
+//                 y: proof.2 .1,
+//             },
+//         };
+//         eth_proof.into()
+//     }
+// }
 
 /// Helper to merkle proof into a bigint vector
 /// TODO: we should create a From trait for this
@@ -96,92 +96,92 @@ pub enum ProofError {
 /// # Errors
 ///
 /// Returns a [`ProofError`] if proving fails.
-pub fn generate_proof(
-    identity: &Identity,
-    merkle_proof: &merkle_tree::Proof<PoseidonHash>,
-    external_nullifier_hash: Field,
-    signal_hash: Field,
-) -> Result<Proof, ProofError> {
-    generate_proof_rng(
-        identity,
-        merkle_proof,
-        external_nullifier_hash,
-        signal_hash,
-        &mut thread_rng(),
-    )
-}
+// pub fn generate_proof(
+//     identity: &Identity,
+//     merkle_proof: &merkle_tree::Proof<PoseidonHash>,
+//     external_nullifier_hash: Field,
+//     signal_hash: Field,
+// ) -> Result<Proof, ProofError> {
+//     generate_proof_rng(
+//         identity,
+//         merkle_proof,
+//         external_nullifier_hash,
+//         signal_hash,
+//         &mut thread_rng(),
+//     )
+// }
 
 /// Generates a semaphore proof from entropy
 ///
 /// # Errors
 ///
 /// Returns a [`ProofError`] if proving fails.
-pub fn generate_proof_rng(
-    identity: &Identity,
-    merkle_proof: &merkle_tree::Proof<PoseidonHash>,
-    external_nullifier_hash: Field,
-    signal_hash: Field,
-    rng: &mut impl Rng,
-) -> Result<Proof, ProofError> {
-    generate_proof_rs(
-        identity,
-        merkle_proof,
-        external_nullifier_hash,
-        signal_hash,
-        ark_bn254::Fr::rand(rng),
-        ark_bn254::Fr::rand(rng),
-    )
-}
+// pub fn generate_proof_rng(
+//     identity: &Identity,
+//     merkle_proof: &merkle_tree::Proof<PoseidonHash>,
+//     external_nullifier_hash: Field,
+//     signal_hash: Field,
+//     rng: &mut impl Rng,
+// ) -> Result<Proof, ProofError> {
+//     generate_proof_rs(
+//         identity,
+//         merkle_proof,
+//         external_nullifier_hash,
+//         signal_hash,
+//         ark_bn254::Fr::rand(rng),
+//         ark_bn254::Fr::rand(rng),
+//     )
+// }
 
-fn generate_proof_rs(
-    identity: &Identity,
-    merkle_proof: &merkle_tree::Proof<PoseidonHash>,
-    external_nullifier_hash: Field,
-    signal_hash: Field,
-    r: ark_bn254::Fr,
-    s: ark_bn254::Fr,
-) -> Result<Proof, ProofError> {
-    let inputs = [
-        ("identityNullifier", vec![identity.nullifier]),
-        ("identityTrapdoor", vec![identity.trapdoor]),
-        ("treePathIndices", merkle_proof.path_index()),
-        ("treeSiblings", merkle_proof_to_vec(merkle_proof)),
-        ("externalNullifier", vec![external_nullifier_hash]),
-        ("signalHash", vec![signal_hash]),
-    ];
-    let inputs = inputs.into_iter().map(|(name, values)| {
-        (
-            name.to_string(),
-            values.iter().map(Into::into).collect::<Vec<_>>(),
-        )
-    });
+// fn generate_proof_rs(
+//     identity: &Identity,
+//     merkle_proof: &merkle_tree::Proof<PoseidonHash>,
+//     external_nullifier_hash: Field,
+//     signal_hash: Field,
+//     r: ark_bn254::Fr,
+//     s: ark_bn254::Fr,
+// ) -> Result<Proof, ProofError> {
+//     let inputs = [
+//         ("identityNullifier", vec![identity.nullifier]),
+//         ("identityTrapdoor", vec![identity.trapdoor]),
+//         ("treePathIndices", merkle_proof.path_index()),
+//         ("treeSiblings", merkle_proof_to_vec(merkle_proof)),
+//         ("externalNullifier", vec![external_nullifier_hash]),
+//         ("signalHash", vec![signal_hash]),
+//     ];
+//     let inputs = inputs.into_iter().map(|(name, values)| {
+//         (
+//             name.to_string(),
+//             values.iter().map(Into::into).collect::<Vec<_>>(),
+//         )
+//     });
 
-    let now = Instant::now();
+//     let now = Instant::now();
 
-    let full_assignment = witness_calculator()
-        .lock()
-        .expect("witness_calculator mutex should not get poisoned")
-        .calculate_witness_element::<Bn254, _>(inputs, false)
-        .map_err(ProofError::WitnessError)?;
+//     let full_assignment = witness_calculator()
+//         .lock()
+//         .expect("witness_calculator mutex should not get poisoned")
+//         .calculate_witness_element::<Bn254, _>(inputs, false)
+//         .map_err(ProofError::WitnessError)?;
 
-    println!("witness generation took: {:.2?}", now.elapsed());
+//     println!("witness generation took: {:.2?}", now.elapsed());
 
-    let now = Instant::now();
-    let zkey = zkey();
-    let ark_proof = create_proof_with_reduction_and_matrices::<_, CircomReduction>(
-        &zkey.0,
-        r,
-        s,
-        &zkey.1,
-        zkey.1.num_instance_variables,
-        zkey.1.num_constraints,
-        full_assignment.as_slice(),
-    )?;
-    let proof = ark_proof.into();
-    println!("proof generation took: {:.2?}", now.elapsed());
+//     let now = Instant::now();
+//     let zkey = zkey();
+//     let ark_proof = create_proof_with_reduction_and_matrices::<_, CircomReduction>(
+//         &zkey.0,
+//         r,
+//         s,
+//         &zkey.1,
+//         zkey.1.num_instance_variables,
+//         zkey.1.num_constraints,
+//         full_assignment.as_slice(),
+//     )?;
+//     let proof = ark_proof.into();
+//     println!("proof generation took: {:.2?}", now.elapsed());
 
-    Ok(proof)
-}
+//     Ok(proof)
+// }
 
 /// Verifies a given semaphore proof
 ///
@@ -189,25 +189,25 @@ fn generate_proof_rs(
 ///
 /// Returns a [`ProofError`] if verifying fails. Verification failure does not
 /// necessarily mean the proof is incorrect.
-pub fn verify_proof(
-    root: Field,
-    nullifier_hash: Field,
-    signal_hash: Field,
-    external_nullifier_hash: Field,
-    proof: &Proof,
-) -> Result<bool, ProofError> {
-    let zkey = zkey();
-    let pvk = prepare_verifying_key(&zkey.0.vk);
+// pub fn verify_proof(
+//     root: Field,
+//     nullifier_hash: Field,
+//     signal_hash: Field,
+//     external_nullifier_hash: Field,
+//     proof: &Proof,
+// ) -> Result<bool, ProofError> {
+//     let zkey = zkey();
+//     let pvk = prepare_verifying_key(&zkey.0.vk);
 
-    let public_inputs = [root, nullifier_hash, signal_hash, external_nullifier_hash]
-        .iter()
-        .map(Fr::try_from)
-        .collect::<Result<Vec<_>, _>>()?;
+//     let public_inputs = [root, nullifier_hash, signal_hash, external_nullifier_hash]
+//         .iter()
+//         .map(Fr::try_from)
+//         .collect::<Result<Vec<_>, _>>()?;
 
-    let ark_proof = (*proof).into();
-    let result = ark_groth16::verify_proof(&pvk, &ark_proof, &public_inputs[..])?;
-    Ok(result)
-}
+//     let ark_proof = (*proof).into();
+//     let result = ark_groth16::verify_proof(&pvk, &ark_proof, &public_inputs[..])?;
+//     Ok(result)
+// }
 
 #[cfg(test)]
 mod test {
@@ -217,71 +217,71 @@ mod test {
     use rand_chacha::ChaChaRng;
     use serde_json::json;
 
-    fn arb_proof(seed: u64) -> Proof {
-        // Deterministic randomness for testing
-        let mut rng = ChaChaRng::seed_from_u64(seed);
+    // fn arb_proof(seed: u64) -> Proof {
+    //     // Deterministic randomness for testing
+    //     let mut rng = ChaChaRng::seed_from_u64(seed);
 
-        // generate identity
-        let seed: [u8; 16] = rng.gen();
-        let id = Identity::from_seed(&seed);
+    //     // generate identity
+    //     let seed: [u8; 16] = rng.gen();
+    //     let id = Identity::from_seed(&seed);
 
-        // generate merkle tree
-        let leaf = Field::from(0);
-        let mut tree = PoseidonTree::new(21, leaf);
-        tree.set(0, id.commitment());
+    //     // generate merkle tree
+    //     let leaf = Field::from(0);
+    //     let mut tree = PoseidonTree::new(21, leaf);
+    //     tree.set(0, id.commitment());
 
-        let merkle_proof = tree.proof(0).expect("proof should exist");
+    //     let merkle_proof = tree.proof(0).expect("proof should exist");
 
-        let external_nullifier: [u8; 16] = rng.gen();
-        let external_nullifier_hash = hash_to_field(&external_nullifier);
+    //     let external_nullifier: [u8; 16] = rng.gen();
+    //     let external_nullifier_hash = hash_to_field(&external_nullifier);
 
-        let signal: [u8; 16] = rng.gen();
-        let signal_hash = hash_to_field(&signal);
+    //     let signal: [u8; 16] = rng.gen();
+    //     let signal_hash = hash_to_field(&signal);
 
-        generate_proof_rng(
-            &id,
-            &merkle_proof,
-            external_nullifier_hash,
-            signal_hash,
-            &mut rng,
-        )
-        .unwrap()
-    }
+    //     generate_proof_rng(
+    //         &id,
+    //         &merkle_proof,
+    //         external_nullifier_hash,
+    //         signal_hash,
+    //         &mut rng,
+    //     )
+    //     .unwrap()
+    // }
 
-    #[test]
-    fn test_proof_cast_roundtrip() {
-        let proof = arb_proof(123);
-        let ark_proof: ArkProof<Bn<Parameters>> = proof.into();
-        let result: Proof = ark_proof.into();
-        assert_eq!(proof, result);
-    }
+    // #[test]
+    // fn test_proof_cast_roundtrip() {
+    //     let proof = arb_proof(123);
+    //     let ark_proof: ArkProof<Bn<Parameters>> = proof.into();
+    //     let result: Proof = ark_proof.into();
+    //     assert_eq!(proof, result);
+    // }
 
-    #[test]
-    fn test_proof_serialize() {
-        let proof = arb_proof(456);
-        let json = serde_json::to_value(&proof).unwrap();
-        assert_eq!(
-            json,
-            json!([
-                [
-                    "0x249ae469686987ee9368da60dd177a8c42891c02f5760e955e590c79d55cfab2",
-                    "0xf22e25870f49388459d388afb24dcf6ec11bb2d4def1e2ec26d6e42f373aad8"
-                ],
-                [
-                    [
-                        "0x17bd25dbd7436c30ea5b8a3a47aadf11ed646c4b25cc14a84ff8cbe0252ff1f8",
-                        "0x1c140668c56688367416534d57b4a14e5a825efdd5e121a6a2099f6dc4cd277b"
-                    ],
-                    [
-                        "0x26a8524759d969ea0682a092cf7a551697d81962d6c998f543f81e52d83e05e1",
-                        "0x273eb3f796fd1807b9df9c6d769d983e3dabdc61677b75d48bb7691303b2c8dd"
-                    ]
-                ],
-                [
-                    "0x62715c53a0eb4c46dbb5f73f1fd7449b9c63d37c1ece65debc39b472065a90f",
-                    "0x114f7becc66f1cd7a8b01c89db8233622372fc0b6fc037c4313bca41e2377fd9"
-                ]
-            ])
-        );
-    }
+    // #[test]
+    // fn test_proof_serialize() {
+    //     let proof = arb_proof(456);
+    //     let json = serde_json::to_value(&proof).unwrap();
+    //     assert_eq!(
+    //         json,
+    //         json!([
+    //             [
+    //                 "0x249ae469686987ee9368da60dd177a8c42891c02f5760e955e590c79d55cfab2",
+    //                 "0xf22e25870f49388459d388afb24dcf6ec11bb2d4def1e2ec26d6e42f373aad8"
+    //             ],
+    //             [
+    //                 [
+    //                     "0x17bd25dbd7436c30ea5b8a3a47aadf11ed646c4b25cc14a84ff8cbe0252ff1f8",
+    //                     "0x1c140668c56688367416534d57b4a14e5a825efdd5e121a6a2099f6dc4cd277b"
+    //                 ],
+    //                 [
+    //                     "0x26a8524759d969ea0682a092cf7a551697d81962d6c998f543f81e52d83e05e1",
+    //                     "0x273eb3f796fd1807b9df9c6d769d983e3dabdc61677b75d48bb7691303b2c8dd"
+    //                 ]
+    //             ],
+    //             [
+    //                 "0x62715c53a0eb4c46dbb5f73f1fd7449b9c63d37c1ece65debc39b472065a90f",
+    //                 "0x114f7becc66f1cd7a8b01c89db8233622372fc0b6fc037c4313bca41e2377fd9"
+    //             ]
+    //         ])
+    //     );
+    // }
 }
